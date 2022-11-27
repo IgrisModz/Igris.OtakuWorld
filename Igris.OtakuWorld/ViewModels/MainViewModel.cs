8ac9773d306e7fd8738762dea7e12d290fea15b9
cyril vanborre <kirito59320@gmail.com>
2022-11-27 12:49:44 +0100

    Add MainViewModel to DataContext

---
+++ Igris.OtakuWorld/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Igris.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igris.OtakuWorld.Creation.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
