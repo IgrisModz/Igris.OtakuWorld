1b2ca6479fb4f5d1281876a286c28fc1155500f4
cyril vanborre <kirito59320@gmail.com>
2022-12-04 21:41:31 +0100

    Add list

---
+++ Igris.OtakuWorld/ViewModels/SeriesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Igris.Mvvm;
+using Igris.OtakuWorld.Creation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igris.OtakuWorld.Creation.ViewModels
+{
+    internal class SeriesViewModel : ViewModelBase
+    {
+        public List<Serie> Series { get; set; }
+
+        public SeriesViewModel()
+        {
+            Series = new List<Serie>
+            {
+                new Serie
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Author= "Moi",
+                    Description = "Ceci est une description"
+                }
+            };
+        }
+    }
+}
--- Igris.OtakuWorld/Views/SeriesView.xaml.cs
+++ Igris.OtakuWorld/Views/SeriesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Igris.OtakuWorld.Creation.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Igris.OtakuWorld.Creation.Views
     {
         public SeriesView()
         {
+            DataContext = new SeriesViewModel();
             InitializeComponent();
         }
     }
