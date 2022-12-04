a4e476ac1ca8d0512906499ec2f7426f64d768f6
cyril vanborre <kirito59320@gmail.com>
2022-12-04 21:41:18 +0100

    Add new objects for all the options

---
+++ Igris.OtakuWorld/Models/Chapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Igris.OtakuWorld.Creation.Models
+{
+    public class Chapter
+    {
+        public float Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime ReleaseDate { get; set; }
+        public DateTime LastCustomDate { get; set; }
+
+        public override string ToString()
+        {
+            return !string.IsNullOrEmpty(Name) ? $"Chapter {Id}: {Name}" : $"Chapter {Id}";
+        }
+    }
+}
---
+++ Igris.OtakuWorld/Models/Serie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace Igris.OtakuWorld.Creation.Models
+{
+    public class Serie
+    {
+        public int Id { get; set; }
+        public BitmapImage Cover { get; set; } = new BitmapImage();
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public List<Chapter> Chapters { get; set; } = new List<Chapter>();
+        public DateTime ReleaseDate { get; set; }
+        public DateTime LastCustomDate { get; set; }
+
+        public override string ToString()
+        {
+            return !string.IsNullOrEmpty(Name) ? Name: Id.ToString();
+        }
+    }
+}
