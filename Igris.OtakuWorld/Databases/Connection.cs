7302393e3a866670c4f20f16093e2b1f910b7286
cyril vanborre <kirito59320@gmail.com>
2022-12-07 22:55:34 +0100

    Add database connection and database function class

---
+++ Igris.OtakuWorld/Databases/Connection.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Igris.OtakuWorld.Creation.Databases
+{
+    internal sealed class Connection
+    {
+        private static MySqlConnection _instance;
+        private readonly static object _lock = new();
+
+        private Connection() { }
+
+        public static MySqlConnection Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instance ??= new MySqlConnection(@$"server=localhost;uid=root;pwd=null;database=otakuworld");
+                }
+            }
+        }
+    }
+}
---
+++ Igris.OtakuWorld/Databases/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igris.OtakuWorld.Creation.Databases
+{
+    public sealed class Database
+    {
+        private static Database _instance;
+        private static readonly object _lock = new object();
+
+        private Database() { }
+
+        public static Database Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instance ??= new Database();
+                }
+            }
+        }
+    }
+}
